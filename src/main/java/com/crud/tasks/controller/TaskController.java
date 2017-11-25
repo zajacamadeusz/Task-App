bededc0951e17ed3e18e062c930f8d634e2574ab
zajacamadeusz <zajacamadeusz7@gmail.com>
2017-11-26 00:59:31 +0100

    modify

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/v1/task")
 public class TaskController {
     @Autowired
