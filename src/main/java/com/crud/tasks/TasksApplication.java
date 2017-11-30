c3d88172a83df2f813d4ce83ff4911fb9a931a24
zajacamadeusz <zajacamadeusz7@gmail.com>
2017-12-01 00:12:58 +0100

    Revert "commit"
    This reverts commit eced479a319efe81d9bc7836ae4790d9dc437466.

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -9,4 +9,5 @@ public class TasksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
-}
+	}
+
--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -1,15 +1,4 @@
 package com.crud.tasks.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-
-@Configuration
 public class CoreConfiguration {
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
 }
