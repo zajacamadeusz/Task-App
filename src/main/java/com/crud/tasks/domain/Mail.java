b00a3ee60288e7afda5eb53b6cecc2d4c12985a4
zajacamadeusz <zajacamadeusz7@gmail.com>
2017-12-12 11:57:51 +0100

    modify

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,25 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+public class Mail {
+
+    private String mailTo;
+    private String subject;
+    private String message;
+    private String toCC;
+
+    public Mail(String mailTo, String subject, String message) {
+        this.mailTo = mailTo;
+        this.subject = subject;
+        this.message = message;
+    }
+
+    public void setCc(String cc) {
+        this.toCC = cc;
+    }
+}
