abf6b0e7c19d1804c4d55b216af50cd519c640dd
zajacamadeusz <zajacamadeusz7@gmail.com>
2018-01-31 23:16:32 +0100

    modify

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -25,12 +25,12 @@ public class TaskController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/tasks/{taskId}")
-    public TaskDto getTask(@RequestParam Long taskId)  throws TaskNotFoundException{
+    public TaskDto getTask(@PathVariable Long taskId)  throws TaskNotFoundException{
         return taskMapper.mapToTaskDto(service.getTask(taskId).orElseThrow(TaskNotFoundException::new));
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/tasks/{taskId}")
-    public void deleteTask(@RequestParam Long taskId) {
+    public void deleteTask(@PathVariable Long taskId) {
         service.deleteTask(taskId);
     }
 
