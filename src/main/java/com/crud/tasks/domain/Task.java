408e78ba0b1e3b34d7ec3cec4f2fbda22c74ddf7
Amadeusz Zajac <zajacamadeusz7@gmail.com>
2019-06-19 22:31:01 +0200

    configuration modified + task dto class name changed

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -16,10 +16,6 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    @Column(name = "name")
     private String title;
-
-    @Column(name = "content")
     private String content;
 }
--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -3,9 +3,11 @@ package com.crud.tasks.domain;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode
 public class TaskDto {
     private Long id;
