b79fcf7d5fb8c2a619b289e3d2a9eb8d85f11769
Amadeusz Zajac <zajacamadeusz7@gmail.com>
2019-06-27 17:55:18 +0200

    trellocarddto class annotation json added

--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -1,5 +1,7 @@
 package com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -9,10 +11,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloCardDto {
-
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("description")
     private String description;
+
+    @JsonProperty("pos")
     private String pos;
+
+    @JsonProperty("listId")
     private String listId;
 }
